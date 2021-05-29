abf8089f2ead67329a5dd650f54b30d14f123ac2
Akanksha Ghildiyal <aghildiyal@dminc.com>
2021-05-29 16:42:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./style.css";
 
-function App() {
+export default function App() {
+  const [expense, setExpense] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [transaction, setTransaction] = useState([]);
+
+  //method to create current date and time
+  const createDate = () => {
+    let date = new Date();
+    return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  };
+
+  //method to add expense
+  const handleAddExpense = () => {
+    if (expense > 0) {
+      setTransaction([
+        ...transaction,
+        { timeAndDate: createDate(), expense: expense, status: "Added" },
+      ]);
+      setTotal(total + parseInt(expense));
+      setExpense(0);
+    }
+  };
+
+  //method to remove expense
+  const handleRemoveExpense = () => {
+    if (total >= expense && expense > 0) {
+      setTransaction([
+        ...transaction,
+        { timeAndDate: createDate(), expense: expense, status: "Removed" },
+      ]);
+      setTotal(total - parseInt(expense));
+      setExpense(0);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className="title">Expense Tracking Basic</h3>
+      <div className="container-add-expense">
+        <div className="total-expense">{`Balance : ${total}`}</div>
+        <input
+          type="number"
+          placeholder="Enter expense"
+          value={expense}
+          onChange={(e) => {
+            setExpense(e.target.value);
+          }}
+        />
+        <div className="container-button">
+          <button onClick={handleAddExpense}>Add</button>
+          <button onClick={handleRemoveExpense}>Remove</button>
+        </div>
+      </div>
+      <ul className="container-transaction">
+        <h4>Transaction</h4>
+        {transaction.map((item, index) => (
+          <li
+            className="list-item"
+            key={index}
+          >{`${item.timeAndDate} ${item.expense} - ${item.status}`}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
